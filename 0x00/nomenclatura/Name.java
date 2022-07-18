197ee9716eb55154dd29b70d8a61f9515e563f15
root <root@lnb036055cps.ciandt.global>
2022-07-18 15:16:46 -0300

    0. Uso da Nomenclatura

--- 0x00/nomenclatura/Name.java
+++ 0x00/nomenclatura/Name.java
@@ -3,25 +3,16 @@ public class Name {
     //A
     public Date modifiedDate;
     public List<Testcase> findAllTestcasesByUser(Name.User user){};
-    //B
-    public Date d; // modified date
-    public List<Testcase> find(Name.User user){}; //find all Testcases by User
 
     //Declarações corretas
     //A
     public List<Account> AccountList;
-    //B
-//    public Account[] AccountList;
 
     // Sem Prefixos
-    //A
-//    public void deleteUser(Name.User pUser){};
     //B
     public void deleteUser(Name.User user){};
 
     // Evite mapa mental
-    //A
-//    public URL r;
     //B
     public URL url;
 
@@ -29,21 +20,12 @@ public class Name {
     //A
     public class TableRepresent {};
     public void saveCar(){};
-    //B
-//    public class TableRepresenter{};
-//    public void car(){};
 
     // Uma palavra por conceito
     //A
     public saveUser(){}; saveAccount(){}; saveContract(){};
-    //B
-//    public saveUser(){}; createAccount(){}; generateContract(){};
-
 
     //Use constants, enums instead of magic numbers, abbreviations
-    //A
-//    public int daysInYear = 360;
-//    public int hoursInWeek = 168;
     //B
     public final int DAYS_IN_YEAR = 360;
     public final int HOURS_IN_WEEK = 168;
